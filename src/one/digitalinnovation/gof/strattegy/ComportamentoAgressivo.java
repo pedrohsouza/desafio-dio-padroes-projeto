8322db71474478e3d26f60722e3ff235a63ce950
pedrohsouza <pedrosouza185@gmail.com>
2023-01-08 23:44:23 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/strattegy/ComportamentoAgressivo.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.gof.strattegy;
+
+public class ComportamentoAgressivo implements Comportamento {
+
+  @Override
+  public void mover() {
+    System.out.println("Movendo-se agressivamente...");
+  }
+
+}
